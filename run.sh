929bc41ea52928c7d3c1a33ef47597bef30bc9f7
alexandermichels <michels9@illinois.edu>
2021-09-09 10:24:33 -0500

    trying to use nfs

---
+++ run.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+MPLBACKEND=Agg 
+# source lmod
+source /etc/profile.d/z00_lmod.sh
+# path on keeling
+export CIGI_EB_ROOT="/data/cigi/cybergisx-easybuild"
+# use the modules on keeling
+module use $CIGI_EB_ROOT/easybuild/modules/all
+module use $CIGI_EB_ROOT/metamodules
+module load cybergisx
+# init conda
+conda init bash
+source ~/.bashrc
+# source conda env
+conda activate $CIGI_EB_ROOT/conda/cybergisx-py3-2021-08-13
+python main.py
